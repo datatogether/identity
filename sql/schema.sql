a7d6b960f3316b1a9fb5694a0a8725c95e40c8ab
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-04-17 11:48:22 -0400

    fix dem tests

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,66 +1,66 @@
 -- name: drop-all
-DROP TABLE IF EXISTS user_keys, keys, users, reset_tokens CASCADE;
+DROP TABLE IF EXISTS user_keys, keys, users, reset_tokens, groups, group_users CASCADE;
 
 -- name: create-users
 CREATE TABLE users (
   id                 UUID PRIMARY KEY,
-  created           integer NOT NULL,
-  updated           integer NOT NULL,
+  created            integer NOT NULL,
+  updated            integer NOT NULL,
   username           text UNIQUE NOT NULL,
   type               integer NOT NULL,
-  password_hash     bytea NOT NULL,
-  email             text UNIQUE NOT NULL,
+  password_hash      bytea NOT NULL,
+  email              text UNIQUE NOT NULL,
   name               text default '',
-  description       text default '',
+  description        text default '',
   home_url           text default '',
-  email_confirmed   boolean DEFAULT false,
+  email_confirmed    boolean DEFAULT false,
   is_admin           boolean DEFAULT false,
-  current_key       text NOT NULL default '',
+  current_key        text NOT NULL default '',
   access_token       text UNIQUE NOT NULL,
-  deleted           boolean DEFAULT false
+  deleted            boolean DEFAULT false
 );
 
 -- name: create-keys
 CREATE TABLE keys (
   type               text NOT NULL,
-  sha_256           bytea PRIMARY KEY,
-  created           integer NOT NULL,
-  last_seen         integer NOT NULL,
+  sha_256            bytea PRIMARY KEY,
+  created            integer NOT NULL,
+  last_seen          integer NOT NULL,
   name               text,
-  user_id           UUID NOT NULL,
+  user_id            UUID NOT NULL,
   public_bytes       bytea NOT NULL,
-  private_bytes     bytea,
-  deleted           boolean DEFAULT false
+  private_bytes      bytea,
+  deleted            boolean DEFAULT false
 );
 
 -- name: create-reset_tokens
 CREATE TABLE reset_tokens (
   id                 UUID PRIMARY KEY,
-  created           integer NOT NULL,
-  updated           integer NOT NULL,
-  email             text NOT NULL,
+  created            integer NOT NULL,
+  updated            integer NOT NULL,
+  email              text NOT NULL,
   used               boolean DEFAULT false
 );
 
 -- name: create-groups
 CREATE TABLE groups (
-  id                UUID PRIMARY KEY,
-  created           integer NOT NULL,
-  updated           integer NOT NULL,
-  creator_id        UUID NOT NULL references users(id),
-  title             text NOT NULL default '',
-  description       text NOT NULL default '',
-  color             text NOT NULL default '#999999',
-  profile_url       text NOT NULL default '',
-  poster_url        text NOT NULL default ''
+  id                 UUID PRIMARY KEY,
+  created            integer NOT NULL,
+  updated            integer NOT NULL,
+  creator_id         UUID NOT NULL references users(id),
+  title              text NOT NULL default '',
+  description        text NOT NULL default '',
+  color              text NOT NULL default '#999999',
+  profile_url        text NOT NULL default '',
+  poster_url         text NOT NULL default ''
 );
 
 -- name: create-group_users
 CREATE TABLE group_users (
-  group_id          UUID NOT NULL references groups(id),
-  user_id           UUID NOT NULL references users(id),
-  invited_by        UUID NOT NULL references users(id),
-  role              text NOT NULL default 'member',
-  joined            timestamp,
-  PRIMARY KEY (group_id, user_id)
+  group_id            UUID NOT NULL references groups(id),
+  user_id             UUID NOT NULL references users(id),
+  invited_by          UUID NOT NULL references users(id),
+  role                text NOT NULL default 'member',
+  joined              timestamp,
+  PRIMARY KEY         (group_id, user_id)
 );
--- sql/test_data.sql
+++ sql/test_data.sql
@@ -1,5 +1,9 @@
+-- name: delete-users
+delete from users;
 -- name: insert-users
-INSERT INTO users VALUES
+INSERT INTO users
+  (id,created,updated,username,type,password_hash,email,name,description,home_url,email_confirmed,is_admin,access_token,deleted)
+VALUES
   -- id, created, updated, handle, type, password_hash, email, name, description, home_url, email_confirmed, access_token, deleted
   ('3fe7d2cc-a8dc-4da0-ac37-c3061d067ae7', 1464282748, 1464282748, 'brendan', 1, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_brendan@qri.io', 'Brendan O''Brien (test user)', '', 'http://brendan.nyc', true, true, '1234567890ABCDE', false),
   ('54b80e91-cae0-423d-b5d8-c9acbb5e2536', 1463687282, 1463687793, 'janelle', 1, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_janelle@qri.io',  'Janelle (test user)', '', 'http://janelle.co', false, false, 'ABCDEFGHIJKLMNO', false),
@@ -7,18 +11,15 @@ INSERT INTO users VALUES
   ('0232fb99-f965-4fe5-bec9-ad099760ab29', 1464282748, 1464282748, 'us_atf', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_us_atf@qri.io','US Dpt. of Alcohol, Tobacco, and Firearms (test user)', 'The United States Census','http://atf.gov', false, false, 'C4d1A2B35e6f7G8', false),
   ('57013bf0-2366-11e6-b67b-9e71128cae77', 1464282748, 1464282748, 'us_census', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_us_census@qri.io','United States Census (test user)', 'The United States Census','http://census.gov', false, false, 'A1B2C3D4E5F6G7H', false);
 
+-- name: delete-reset_tokens
+delete from reset_tokens;
 -- name: insert-reset_tokens
 INSERT INTO reset_tokens VALUES
   ('69eb9cbd-7085-4624-a841-59d0f02eaa7b', 1464282748, 1464282748, 'test_user_brendan@qri.io', false);
 
+-- name: delete-keys
+delete from keys;
 -- name: insert-keys
 INSERT INTO keys VALUES
   -- type, sha_256, created, last_seen, name, user_id, public_bytes, private_bytes, deleted
   ('rsa', '\x',1464282748,1464282748,'stuff','61e91231-c7cc-47b4-b392-89fb180a7570', '\x', '\x', false);
-
--- name: delete-users
-delete from users;
--- name: delete-reset_tokens
-delete from reset_tokens;
--- name: delete-keys
-delete from keys;
