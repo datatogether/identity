55c2d22dedfe64389aba368b7282754d3480aaed
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-05-25 15:11:25 -0400

    moar config refactor

--- config.go
+++ config.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	conf "github.com/archivers-space/config"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 // server modes
@@ -18,72 +16,71 @@ const (
 
 // config holds all configuration for the server. It pulls from three places (in order):
 // 		1. environment variables
-// 		2. config.[server_mode].json <- eg: config.test.json
-// 		3. config.json
+// 		2. .[MODE].env OR .env
 //
-// env variables win, but can only set config who's json is ALL_CAPS
-// it's totally fine to not have, say, config.develop.json defined, and just
-// rely on a base config.json. But if you're in production mode & config.production.json
-// exists, that will be read *instead* of config.json.
+// globally-set env variables win.
+// it's totally fine to not have, say, .env.develop defined, and just
+// rely on a base ".env" file. But if you're in production mode & ".env.production"
+// exists, that will be read *instead* of .env
 //
 // configuration is read at startup and cannot be alterd without restarting the server.
 type config struct {
 	// configurable environment variable
-	Environment string `json:"GOLANG_ENV"`
+	Environment string
 
 	// port to listen on, will be read from PORT env variable if present.
-	Port string `json:"PORT"`
+	Port string
 
 	// root url for service
-	UrlRoot string `json:"URL_ROOT"`
+	UrlRoot string
 
 	// url of postgres app db. required.
-	PostgresDbUrl string `json:"POSTGRES_DB_URL"`
+	PostgresDbUrl string
 
 	// secret for encrypted session cookies. required.
-	SessionSecret string `json:"SESSION_SECRET"`
+	SessionSecret string
 
 	// Private Key to use for signing thinks like JWTs
 	// to create: openssl genrsa -out app.rsa keysize
-	PrivateKey string `json:"PRIVATE_KEY"`
+	PrivateKey string
 	// Public Key to use for signing things like JWTs
 	// openssl rsa -in app.rsa -pubout > app.rsa.pub
-	PublicKey string `json:"PUBLIC_KEY"`
+	PublicKey string
 
 	// TLS (HTTPS) enable support via LetsEncrypt, default false
 	// should be true in production
-	TLS bool `json:"TLS"`
+	TLS bool
 
 	// because identity is an api-only server, FrontendUrl specifies
 	// a user-friendly URL to redirect to
-	FrontendUrl string `json:"FRONTEND_URL"`
+	FrontendUrl string
 
 	// Key to store user's cookie under
-	UserCookieKey string `json:"USER_COOKIE_KEY"`
+	UserCookieKey string
 	// domain to attach cookie to.
-	UserCookieDomain string `json:"USER_COOKIE_DOMAIN"`
+	UserCookieDomain string
 
 	// support CORS signing from a list of origins
-	AllowedOrigins []string `json:"ALLOWED_ORIGINS"`
+	AllowedOrigins []string
 
 	// setting HTTP_AUTH_USERNAME & HTTP_AUTH_PASSWORD
 	// will enable basic http auth for the server. This is a single
 	// username & password that must be passed in with every request.
 	// leaving these values blank will disable http auth
 	// read from env variable: HTTP_AUTH_USERNAME
-	HttpAuthUsername string `json:"HTTP_AUTH_USERNAME"`
+	HttpAuthUsername string
 	// read from env variable: HTTP_AUTH_PASSWORD
-	HttpAuthPassword string `json:"HTTP_AUTH_PASSWORD"`
+	HttpAuthPassword string
 
 	// if true, requests that have X-Forwarded-Proto: http will be redirected
 	// to their https variant
-	ProxyForceHttps bool `json:"PROXY_FORCE_HTTPS"`
+	ProxyForceHttps bool
 	// CertbotResponse is only for doing manual SSL certificate generation
 	// via LetsEncrypt.
-	CertbotResponse string `json:"CERTBOT_RESPONSE"`
+	CertbotResponse string
 
-	GithubAppId     string `json:"GITHUB_APP_ID"`
-	GithubAppSecret string `json:"GITHUB_APP_SECRET"`
+	GithubAppId     string
+	GithubAppSecret string
 }
 
 // initConfig pulls configuration from config.json
@@ -93,27 +90,6 @@ func initConfig(mode string) (cfg *config, err error) {
 	if err := loadConfigFile(mode, cfg); err != nil {
 		return cfg, err
 	}
-
-	// override config settings with env settings, passing in the current configuration
-	// as the default. This has the effect of leaving the config.json value unchanged
-	// if the env variable is empty
-	cfg.Port = readEnvString("PORT", cfg.Port)
-	cfg.UrlRoot = readEnvString("URL_ROOT", cfg.UrlRoot)
-	cfg.PublicKey = readEnvString("PUBLIC_KEY", cfg.PublicKey)
-	cfg.PrivateKey = readEnvString("PRIVATE_KEY", cfg.PrivateKey)
-	cfg.TLS = readEnvBool("TLS", cfg.TLS)
-	cfg.UrlRoot = readEnvString("SESSION_SECRET", cfg.SessionSecret)
-	cfg.UserCookieKey = readEnvString("USER_COOKIE_KEY", cfg.UserCookieKey)
-	cfg.UserCookieDomain = readEnvString("USER_COOKIE_DOMAIN", cfg.UserCookieDomain)
-	cfg.PostgresDbUrl = readEnvString("POSTGRES_DB_URL", cfg.PostgresDbUrl)
-	cfg.AllowedOrigins = readEnvStringSlice("ALLOWED_ORIGINS", cfg.AllowedOrigins)
-	cfg.HttpAuthUsername = readEnvString("HTTP_AUTH_USERNAME", cfg.HttpAuthUsername)
-	cfg.HttpAuthPassword = readEnvString("HTTP_AUTH_PASSWORD", cfg.HttpAuthPassword)
-	cfg.CertbotResponse = readEnvString("CERTBOT_RESPONSE", cfg.CertbotResponse)
-	cfg.ProxyForceHttps = readEnvBool("PROXY_FORCE_HTTPS", cfg.ProxyForceHttps)
-	cfg.GithubAppId = readEnvString("GITHUB_APP_ID", cfg.GithubAppId)
-	cfg.GithubAppSecret = readEnvString("GITHUB_APP_SECRET", cfg.GithubAppSecret)
-	cfg.FrontendUrl = readEnvString("FRONTEND_URL", cfg.FrontendUrl)
 
 	// make sure port is set
 	if cfg.Port == "" {
@@ -139,37 +115,6 @@ func packagePath(path string) string {
 	return filepath.Join(os.Getenv("GOPATH"), "src/github.com/archivers-space/identity", path)
 }
 
-// readEnvString reads key from the environment, returns def if empty
-func readEnvString(key, def string) string {
-	if env := os.Getenv(key); env != "" {
-		return env
-	}
-	return def
-}
-
-func readEnvBytes(key string, def []byte) []byte {
-	if env := os.Getenv(key); env != "" {
-		return []byte(env)
-	}
-	return def
-}
-
-// readEnvBool read key form the env, converting to a boolean value. returns def if empty
-func readEnvBool(key string, def bool) bool {
-	if env := os.Getenv(key); env != "" {
-		return env == "true" || env == "TRUE" || env == "t"
-	}
-	return def
-}
-
-// readEnvString reads a slice of strings from key environment var, returns def if empty
-func readEnvStringSlice(key string, def []string) []string {
-	if env := os.Getenv(key); env != "" {
-		return strings.Split(env, ",")
-	}
-	return def
-}
-
 // requireConfigStrings panics if any of the passed in values aren't set
 func requireConfigStrings(values map[string]string) error {
 	for key, value := range values {
@@ -184,30 +129,16 @@ func requireConfigStrings(values map[string]string) error {
 // checks for config.[mode].json file to read configuration from if the file exists
 // defaults to config.json, silently fails if no configuration file is present.
 func loadConfigFile(mode string, cfg *config) (err error) {
-	var data []byte
-
-	fileName := packagePath(fmt.Sprintf("config.%s.json", mode))
+	fileName := packagePath(fmt.Sprintf(".%s.env", mode))
 	if !fileExists(fileName) {
-		fileName = "config.json"
+		fileName = ".env"
 		if !fileExists(fileName) {
 			return nil
 		}
 	}
 
-	logger.Printf("reading config file: %s", fileName)
-	data, err = ioutil.ReadFile(fileName)
-	if err != nil {
-		err = fmt.Errorf("error reading %s: %s", fileName, err)
-		return
-	}
-
-	// unmarshal ("decode") config data into a config struct
-	if err = json.Unmarshal(data, cfg); err != nil {
-		err = fmt.Errorf("error parsing %s: %s", fileName, err)
-		return
-	}
-
-	return
+	logger.Printf("loading config file: %s", filepath.Base(fileName))
+	return conf.Load(cfg, fileName)
 }
 
 // Does this file exist?
