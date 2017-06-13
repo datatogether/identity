273bbd233ee5eabf3041482dc50b7dc3540bb7b9
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-06-13 15:28:04 +0100

    moar test work

--- access_token/main_test.go
+++ access_token/main_test.go
@@ -43,7 +43,7 @@ const schema = `
 DROP TABLE IF EXISTS users CASCADE;
 
 -- name: create-users
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   id                 UUID PRIMARY KEY,
   created            integer NOT NULL,
   updated            integer NOT NULL,
@@ -73,6 +73,6 @@ VALUES
   ('54b80e91-cae0-423d-b5d8-c9acbb5e2536', 1463687282, 1463687793, 'janelle', 1, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_janelle@qri.io',  'Janelle (test user)', '', 'http://janelle.co', false, false, 'ABCDEFGHIJKLMNO', false),
   ('1b674f47-d0f4-4b3c-b25d-c49521b5599a', 1464282748, 1464282748, 'ca_census', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_ca_census@qri.io','Canadian Census (test user)', 'Les Census Canadien','http://census.ca', false, false, '1A2B3C4D5E6F7G8', false),
   ('0232fb99-f965-4fe5-bec9-ad099760ab29', 1464282748, 1464282748, 'us_atf', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_us_atf@qri.io','US Dpt. of Alcohol, Tobacco, and Firearms (test user)', 'The United States Census','http://atf.gov', false, false, 'C4d1A2B35e6f7G8', false),
-  ('57013bf0-2366-11e6-b67b-9e71128cae77', 1464282748, 1464282748, 'us_census', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_us_census@qri.io','United States Census (test user)', 'The United States Census','http://census.gov', false, false, 'A1B2C3D4E5F6G7H', false);
-
+  ('57013bf0-2366-11e6-b67b-9e71128cae77', 1464282748, 1464282748, 'us_census', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_us_census@qri.io','United States Census (test user)', 'The United States Census','http://census.gov', false, false, 'A1B2C3D4E5F6G7H', false)
+ON CONFLICT DO NOTHING;
 `
--- user/main_test.go
+++ user/main_test.go
@@ -45,7 +45,7 @@ const schema = `
 DROP TABLE IF EXISTS user_keys, oauth_users, oauth_tokens, keys, users, reset_tokens, groups, group_users CASCADE;
 
 -- name: create-users
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   id                 UUID PRIMARY KEY,
   created            integer NOT NULL,
   updated            integer NOT NULL,
@@ -64,7 +64,7 @@ CREATE TABLE users (
 );
 
 -- name: create-oauth_tokens
-CREATE TABLE oauth_tokens (
+CREATE TABLE IF NOT EXISTS oauth_tokens (
   user_id            UUID NOT NULL references users(id),
   service            text NOT NULL DEFAULT '',
   access_token       text NOT NULL DEFAULT '',
@@ -75,7 +75,7 @@ CREATE TABLE oauth_tokens (
 );
 
 -- name: create-keys
-CREATE TABLE keys (
+CREATE TABLE IF NOT EXISTS keys (
   type               text NOT NULL,
   sha_256            bytea PRIMARY KEY,
   created            integer NOT NULL,
@@ -88,7 +88,7 @@ CREATE TABLE keys (
 );
 
 -- name: create-reset_tokens
-CREATE TABLE reset_tokens (
+CREATE TABLE IF NOT EXISTS reset_tokens (
   id                 UUID PRIMARY KEY,
   created            integer NOT NULL,
   updated            integer NOT NULL,
@@ -108,23 +108,28 @@ VALUES
   ('54b80e91-cae0-423d-b5d8-c9acbb5e2536', 1463687282, 1463687793, 'janelle', 1, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_janelle@qri.io',  'Janelle (test user)', '', 'http://janelle.co', false, false, 'ABCDEFGHIJKLMNO', false),
   ('1b674f47-d0f4-4b3c-b25d-c49521b5599a', 1464282748, 1464282748, 'ca_census', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_ca_census@qri.io','Canadian Census (test user)', 'Les Census Canadien','http://census.ca', false, false, '1A2B3C4D5E6F7G8', false),
   ('0232fb99-f965-4fe5-bec9-ad099760ab29', 1464282748, 1464282748, 'us_atf', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_us_atf@qri.io','US Dpt. of Alcohol, Tobacco, and Firearms (test user)', 'The United States Census','http://atf.gov', false, false, 'C4d1A2B35e6f7G8', false),
-  ('57013bf0-2366-11e6-b67b-9e71128cae77', 1464282748, 1464282748, 'us_census', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_us_census@qri.io','United States Census (test user)', 'The United States Census','http://census.gov', false, false, 'A1B2C3D4E5F6G7H', false);
+  ('57013bf0-2366-11e6-b67b-9e71128cae77', 1464282748, 1464282748, 'us_census', 2, '\x2432612431302447383370444e4f387a6a7350542f33654377423358756c6e787947327534614247436d787445325556314e50397976413432757579', 'test_user_us_census@qri.io','United States Census (test user)', 'The United States Census','http://census.gov', false, false, 'A1B2C3D4E5F6G7H', false)
+ON CONFLICT DO NOTHING;
 
 -- name: delete-reset_tokens
 delete from reset_tokens;
 -- name: insert-reset_tokens
-INSERT INTO reset_tokens VALUES
-  ('69eb9cbd-7085-4624-a841-59d0f02eaa7b', 1464282748, 1464282748, 'test_user_brendan@qri.io', false);
+INSERT INTO reset_tokens
+VALUES
+  ('69eb9cbd-7085-4624-a841-59d0f02eaa7b', 1464282748, 1464282748, 'test_user_brendan@qri.io', false)
+ON CONFLICT DO NOTHING;
 
 -- name: delete-keys
 delete from keys;
 -- name: insert-keys
-INSERT INTO keys VALUES
+INSERT INTO keys 
+VALUES
   -- type, sha_256, created, last_seen, name, user_id, public_bytes, private_bytes, deleted
-  ('rsa', '\x',1464282748,1464282748,'stuff','61e91231-c7cc-47b4-b392-89fb180a7570', '\x', '\x', false);
+  ('rsa', '\x',1464282748,1464282748,'stuff','61e91231-c7cc-47b4-b392-89fb180a7570', '\x', '\x', false)
+ON CONFLICT DO NOTHING;
 
 -- name: delete-oauth_tokens
 DELETE FROM oauth_tokens;
 -- name: insert-oauth_tokens
--- INSERT INTO oauth_tokens VALUES ();
+-- INSERT INTO oauth_tokens VALUES () ON CONFLICT DO NOTHING;
 `
